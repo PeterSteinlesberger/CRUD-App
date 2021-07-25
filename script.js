243f19b181e9211503ef2635cdfe8a9ddb9bc879
PeterSteinlesberger <petersteinlesberger@mail.de>
2021-07-25 21:22:52 +0200

    localStorage function added

--- script.js
+++ script.js
@@ -7,15 +7,34 @@ let newNumber = document.getElementById('inputNumber').value;
 names.push(newName);
 numbers.push(newNumber);
 showData();
+save();
 }
 
 function showData() {
     document.getElementById('data-cont').innerHTML = '';
     for (let i = 0; i < names.length; i++) {
     document.getElementById('data-cont').innerHTML += `
-    <div class="data">
+    <div id="${i}" class="data">
+    <div>
     <div>Name: ${names[i]}</div>
     <div>Number: ${numbers[i]}</div>
+    </div>
+    <button class="button" onclick="deleteData(${i})">Delete</button>
     </div>`;
     }
+}
+
+function deleteData(idOfContainer) {
+ let  i = idOfContainer;
+names.splice(i, 1);
+numbers.splice(i, 1);
+showData();
+save();
+}
+
+function save() {
+    let namesAsText = JSON.stringify(names);
+    localStorage.setItem('names', namesAsText);
+    let numbersAsText = JSON.stringify(numbers);
+    localStorage.setItem('numbers', numbersAsText);
 }
